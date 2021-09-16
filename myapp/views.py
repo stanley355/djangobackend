82a4171dbba9734bdd09472a0edf388f846ea404
stanley355 <winatastanley355@gmail.com>
2021-09-16 08:49:57 +0700

    Models

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
+
 
 # Create your views here.
 def index(request):
-    context ={
-        'name': 'Pat',
-        'age': 25,
-        'country': 'indo'
-    }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.is_true = True
+    feature1.details = 'Our service is good'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Bum'
+    feature2.is_true = False
+    feature2.details = 'Our service is bad'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Bommmmmm'
+    feature3.is_true = True
+    feature3.details = 'Our service is worst'
+
+    features = [feature1, feature2, feature3]
+    
+    for feature in features:
+        pass
+
+    return render(request, 'index.html', {'featureloop': features})
 
 def counter(request):
     text = request.POST['text']
