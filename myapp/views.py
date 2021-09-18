ccfea0920202a7b97727d9a117e47204ec38eb8e
stanley355 <winatastanley355@gmail.com>
2021-09-18 14:23:07 +0700

    Django register

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 
@@ -7,6 +9,32 @@ from .models import Feature
 def index(request):
     features = Feature.objects.all()
     return render(request, 'index.html', {'featureloop': features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Validation
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used!')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'User already added')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username= username, email = email, password = password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password is not same')
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html')
 
 def counter(request):
     text = request.POST['text']
